274f0551811401acb9d3d3a0cd41261d5048529d
dragon <59757419+roosaramendis@users.noreply.github.com>
2021-12-24 20:38:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+#created by: sidhas roosara mendis (DRAGON) 
+#github link:
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import *
+
+import sys
+import py_youtubedownloader_UI, py_settings,info
+
+class info(QtWidgets.QMainWindow, info.Ui_Frame):
+    def __init__(self, parent=None):
+        super(info, self).__init__(parent)
+        self.setupUi(self)
+
+class settings_window(QtWidgets.QMainWindow, py_settings.Ui_Frame):
+    def __init__(self, parent=None):
+        super (settings_window, self).__init__(parent)
+        self.setupUi(self)
+        self.credits.clicked.connect(self.credits_clk1)
+        self.dialog1 = info(self)
+    def credits_clk1(self):
+        self.dialog1.show()    
+class downloader_window(QtWidgets.QMainWindow, py_youtubedownloader_UI.Ui_Form):
+    def __init__(self, parent=None):
+        super(downloader_window, self).__init__(parent)
+        self.setupUi(self)
+        
+        self.pb_settings.clicked.connect(self.on_pushButton_clicked)
+        self.dialog = settings_window(self)
+
+    def on_pushButton_clicked(self):
+        self.dialog.show()
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    main = downloader_window()
+    main.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main() 
